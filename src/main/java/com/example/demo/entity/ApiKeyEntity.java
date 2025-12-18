f816a31f9ef9a5fbfa37993904dde1a016193a21
bala-amypo <bala@amypo.in>
2025-12-18 06:27:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiKeyEntity.java
+++ src/main/java/com/example/demo/entity/ApiKeyEntity.java
@@ -1,20 +1,82 @@
-package com.example.demo.entity
+import java.sql.Timestamp;
 
-public class ApiKeyEntity(){
-    @Id;
-    private Long id;
+public class ApiKeyEntity {
+    
+    private Long id;               
+    private String keyValue;        
+    private Long ownerId;           
+    private QuotaPlan plan;         
+    private Boolean active;         
+    private Timestamp createdAt;    
+    private Timestamp updatedAt;    
 
-
-    private String keyValue;
-    private Long ownerId;
-    private ManyToOne plan;
-    private Boolean active;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
-
-    public class ApiKeyEntity(){
-
+    public ApiKeyEntity(Long id, String keyValue, Long ownerId, QuotaPlan plan, Boolean active, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.keyValue = keyValue;
+        this.ownerId = ownerId;
+        this.plan = plan;
+        this.active = (active == null) ? true : active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
+    public ApiKeyEntity() {
+        this.active = true;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKeyValue() {
+        return keyValue;
+    }
+
+    public void setKeyValue(String keyValue) {
+        this.keyValue = keyValue;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public QuotaPlan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(QuotaPlan plan) {
+        this.plan = plan;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
