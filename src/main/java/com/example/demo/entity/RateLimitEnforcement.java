8fd458ae8c73d139dc89a7e2d76d964696b14a85
bala-amypo <bala@amypo.in>
2025-12-18 09:42:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/RateLimitEnforcement.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+import java.sql.Timestamp;
+
+@Entity
+public class RateLimitEnforcement(){
+    @Id
+    private Long id;
+    private Timestamp blockedAt;
+    private int limitExceededBy;
+    private String message;
+
+    
+    public RateLimitEnforcement() {
+    }
+
+    public RateLimitEnforcement(Long id, Timestamp blockedAt, int limitExceededBy, String message) {
+        this.id = id;
+        this.blockedAt = blockedAt;
+        this.limitExceededBy = limitExceededBy;
+        this.message = message;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Timestamp getBlockedAt() {
+        return blockedAt;
+    }
+    public void setBlockedAt(Timestamp blockedAt) {
+        this.blockedAt = blockedAt;
+    }
+    public int getLimitExceededBy() {
+        return limitExceededBy;
+    }
+    public void setLimitExceededBy(int limitExceededBy) {
+        this.limitExceededBy = limitExceededBy;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
