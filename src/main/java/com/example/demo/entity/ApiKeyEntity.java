78f89c764fd130f3ee18a1fa695aad722266e016
bala-amypo <bala@amypo.in>
2025-12-18 06:25:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiKeyEntity.java
+++ src/main/java/com/example/demo/entity/ApiKeyEntity.java
@@ -1,7 +1,20 @@
 package com.example.demo.entity
 
 public class ApiKeyEntity(){
+    @Id;
     private Long id;
+
+
     private String keyValue;
-    
+    private Long ownerId;
+    private ManyToOne plan;
+    private Boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public class ApiKeyEntity(){
+
+    }
+
+
 }
