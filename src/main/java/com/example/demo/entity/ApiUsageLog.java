0f39c9483ad33ef04596367dfe9fb400f6b8cb2f
bala-amypo <bala@amypo.in>
2025-12-18 09:07:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiUsageLog.java
+++ src/main/java/com/example/demo/entity/ApiUsageLog.java
@@ -1,1 +1,44 @@
 package com.example.demo.entity;
+
+import java.sql.Timestamp;
+import jakarta.persistence.*;
+
+@Entity
+public class ApiUsageLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String endpoint;
+    private Timestamp timestamp;
+
+    
+    
+    public ApiUsageLog() {
+    }
+    public ApiUsageLog(Long id, String endpoint, Timestamp timestamp) {
+        this.id = id;
+        this.endpoint = endpoint;
+        this.timestamp = timestamp;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEndpoint() {
+        return endpoint;
+    }
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+}
