cc3c0c862b2488cc69ae0fdeef78f9b6963322d7
bala-amypo <bala@amypo.in>
2025-12-26 10:22:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiKeyUsageSummaryDto.java
+++ src/main/java/com/example/demo/dto/ApiKeyUsageSummaryDto.java
@@ -1,11 +1,14 @@
 package com.example.demo.dto;
 
-public class AuthRequestDto {
-    private String email;
-    private String password;
+public class ApiKeyUsageSummaryDto {
+    private String keyName;
+    private Long planId;
+    private Long ownerId;
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getKeyName() { return keyName; }
+    public void setKeyName(String keyName) { this.keyName = keyName; }
+    public Long getPlanId() { return planId; }
+    public void setPlanId(Long planId) { this.planId = planId; }
+    public Long getOwnerId() { return ownerId; }
+    public void setOwnerId(Long ownerId) { this.ownerId = ownerId; }
 }
